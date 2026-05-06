625210d6a9abe224a23a017d4f7e2c46c76e66b5
basit shahid <basitdogar59@gmail.com>
2026-05-06 21:43:53 +0500

    Clean up and fix MainActivity.java

--- drop-client/android/app/src/main/java/com/basitshahid/drop/MainActivity.java
+++ drop-client/android/app/src/main/java/com/basitshahid/drop/MainActivity.java
@@ -34,42 +34,6 @@ public class MainActivity extends BridgeActivity {
                 ArrayList<Uri> intentUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
                 if (intentUris != null) uris.addAll(intentUris);
             }
-package com.basitshahid.drop;
-
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.JSObject;
-
-import java.util.ArrayList;
-
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        handleIntent(getIntent());
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-        String action = intent.getAction();
-        String type = intent.getType();
-
-        if ((Intent.ACTION_SEND.equals(action) || Intent.ACTION_SEND_MULTIPLE.equals(action)) && type != null) {
-            ArrayList<Uri> uris = new ArrayList<>();
-            if (Intent.ACTION_SEND.equals(action)) {
-                Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-                if (uri != null) uris.add(uri);
-            } else {
-                ArrayList<Uri> intentUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-                if (intentUris != null) uris.addAll(intentUris);
-            }
 
             if (!uris.isEmpty()) {
                 JSObject ret = new JSObject();
